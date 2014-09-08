5cd2434ac3573010b188fd528e0137f6aac7c7aa
sshuisky <sergeyiih@gmail.com>
2014-09-08 14:06:53 +0400

    looks like finish soon

--- rectangles.py
+++ rectangles.py
@@ -1,14 +1,20 @@
 __author__ = 'january'
 
-print("sdf")
-print('sdfdfs')
+rectangles = [[0, 1, 3, 3], [2, 2, 6, 4], [1, 0, 3, 5]]
+total = 0
+intersections = []
+for i, rectangle in enumerate(rectangles):
+    area = (rectangle[2] - rectangle[0]) * (rectangle[3] - rectangle[1])
+    total += area
 
-friends = ['john', 'pat', 'gary', 'michael']
-for i, name in enumerate(friends):
-    print("iteration {iteration} is {name}".format(iteration=i, name=name))
+for i, rectangle in enumerate(rectangles):
+    remain = rectangles[i + 1:]
+    for j, compare in enumerate(remain):
+        intersection = [max(rectangle[0], compare[0]), max(rectangle[1], compare[1]), min(rectangle[2], compare[2]),
+                        min(rectangle[3], compare[3])]
+        intersection_area = (intersection[2] - intersection[0]) * (intersection[3] - intersection[1])
+        intersections += [intersection]
+        total -= intersection_area
 
-
-parents, babies = (1, 1)
-while babies < 1000:
-    print('This generation has {0} babies'.format(babies))
-    parents, babies = (babies, parents + babies)
+print(intersections)
+print(total)
